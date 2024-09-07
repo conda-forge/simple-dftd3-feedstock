c3cefabbcb53726e47eb95f830109c160a6b3b81
Sebastian Ehlert <sehlert@microsoft.com>
2024-09-07 12:46:00 -0400

    Support Windows

---
+++ recipe/bld.bat
@@ -0,0 +1,18 @@
+@echo on
+setlocal enabledelayedexpansion
+
+mkdir builddir
+
+set "MESON_RSP_THRESHOLD=320000"
+
+meson setup _build %MESON_ARGS%
+if %ERRORLEVEL% neq 0 exit 1
+
+meson compile -C _build
+if %ERRORLEVEL% neq 0 exit 1
+
+meson test -C _build --no-rebuild --print-errorlogs
+if %ERRORLEVEL% neq 0 exit 1
+
+meson install -C _build --no-rebuild
+if %ERRORLEVEL% neq 0 exit 1
