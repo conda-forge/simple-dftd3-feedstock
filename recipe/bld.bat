5633c54dbd5b6e9b9466c30570ce70cbd1b53495
Sebastian Ehlert <sehlert@microsoft.com>
2024-09-08 15:59:06 -0400

    Ensure PKG_CONFIG_PATH is set on Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,6 +4,7 @@ setlocal enabledelayedexpansion
 mkdir builddir
 
 set "MESON_RSP_THRESHOLD=320000"
+set PKG_CONFIG_PATH=%PREFIX%\lib\pkgconfig
 
 meson setup _build %MESON_ARGS%
 if %ERRORLEVEL% neq 0 exit 1
