c6a10b13bccec6325f0047952eaebdfc13aabf11
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2024-09-09 06:34:35 -0400

    Don't use JSON on Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,7 +6,7 @@ mkdir builddir
 set "MESON_RSP_THRESHOLD=320000"
 set PKG_CONFIG_PATH=%PREFIX%\lib\pkgconfig
 
-meson setup _build %MESON_ARGS%
+meson setup _build %MESON_ARGS% -Djson=disabled
 if %ERRORLEVEL% neq 0 exit 1
 
 meson compile -C _build
